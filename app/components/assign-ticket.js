cfeddb5e9e0c2f2816787fc222237f68760bc3e8
abhishek97 <zapper.awake@gmail.com>
2018-10-08 23:42:05 +0530

    add toasts and post redirects

--- app/components/assign-ticket.js
+++ app/components/assign-ticket.js
@@ -1,12 +1,18 @@
 import Ember from 'ember';
+import { inject as service } from '@ember/service'
 
 export default Ember.Component.extend({
-    router: Ember.inject.service('-routing'),
+    notify: service(),
+    router: service(),
     actions : {
         assignTo(){
             this.get('ticket').save().then(ticket=>{
-              this.get('router').transitionTo('ticket');
-            });
+              this.get('notify').success('Ticket Assigned Sucessfully');
+              this.get('router').transitionTo('ticket')
+            }).catch(err => {
+              console.error(err)
+              this.get('notify').error('There was some error');
+            })
         },
     }
 });
--- app/components/show-ticket.js
+++ app/components/show-ticket.js
@@ -1,22 +1,28 @@
 import Ember from 'ember';
-
+import { inject as service } from '@ember/service';
 /*
 0 -> Open
-1 -> Closed by Engineer
-2 -> Closed
+1 -> Assigned to fb
+2 -> Resolved by fb
+3 -> Closed
 */
 export default Ember.Component.extend({
+    notify: service(),
+    router: service(),
     actions : {
         reOpenTicket(){
             this.send('changeStatus', 0);
         },
         closeTicket(){
-            this.send('changeStatus', 2);
+            this.send('changeStatus', 3);
         },
         changeStatus(status){
             const ticket = this.get('ticket');
             ticket.set('status', status);
-            ticket.save();
+            ticket.save().then(() => {
+              this.get('notify').success('Ticket updated successfully')
+              this.get('router').transitionTo('ticket')
+            })
         }
     }
 });

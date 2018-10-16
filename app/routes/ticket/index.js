d4ca4d82a56bc56dc395c1a7d0d6a426d4c06d07
abhishek97 <zapper.awake@gmail.com>
2018-10-16 09:49:31 +0530

    query for tickets based on status

--- app/routes/ticket/index.js
+++ app/routes/ticket/index.js
@@ -2,11 +2,13 @@ import Ember from 'ember';
 import moment from 'moment';
 
 export default Ember.Route.extend({
-    model(){
-        return this.get('store').query('ticket', {
-          filter : {
-            after : moment().subtract(1,'days').format()
-          }
-        });
-    }
+  model() {
+    return this.get('store').query('ticket', {
+      filter: {
+        status: {
+          $ne: 3
+        }
+      }
+    });
+  }
 });

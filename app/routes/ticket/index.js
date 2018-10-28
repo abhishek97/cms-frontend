b4df0542aac0deb6bb97680cb7fcd823701a210a
abhishek97 <zapper.awake@gmail.com>
2018-10-28 11:26:34 +0530

    add auto refresh

--- app/routes/ticket/index.js
+++ app/routes/ticket/index.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
-import moment from 'moment';
+import config from 'cms-frontend/config/environment'
 
 export default Ember.Route.extend({
+  activate () {
+    window.setInterval( () => {
+      this.refresh()
+    }, config.autoRefreshTimeout || 1000)
+  },
   model() {
     return this.get('store').query('ticket', {
       filter: {
